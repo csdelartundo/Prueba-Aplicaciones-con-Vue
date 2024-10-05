3eb80f8925f80577c068dbd75eca31c9ba0aa7d2
Constanza <csdelartundo@gmail.com>
2024-10-05 11:49:17 -0300

    Se crea test para vistas HomeView y AboutView, cada test es almacenado en una carpeta relacionada a su evaluacion

---
+++ tests/unit/views/PruebaRutas.spec.js
@@ -0,0 +1,47 @@
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+import AboutView from '@/views/AboutView.vue'
+import HomeView from '@/views/HomeView.vue'
+
+describe('Vistas HomeView y AboutView', () => {
+    test('Probando existencia de vista HomeView', async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes: [
+                {
+                    path: '/',
+                    name: 'home',
+                    component: HomeView
+                }
+            ]
+        })
+        router.push('/')
+        await router.isReady()
+        const wrapper = mount(HomeView, {
+            global: {
+                plugins: [router]
+            }
+        })
+        expect(wrapper.findComponent(HomeView).exists()).toBe(true)
+    })
+    test('Probando existencia de vista AboutView', async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes: [
+                {
+                    path: '/about',
+                    name: 'about',
+                    component: AboutView
+                }
+            ]
+        })
+        router.push('/')
+        await router.isReady()
+        const wrapper = mount(AboutView, {
+            global: {
+                plugins: [router]
+            }
+        })
+        expect(wrapper.findComponent(AboutView).exists()).toBe(true)
+    })
+})
