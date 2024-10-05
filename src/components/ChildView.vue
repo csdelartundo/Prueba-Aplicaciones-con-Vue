5f5219ab50dcd6ae9a4839981736a202bc60daec
Constanza <csdelartundo@gmail.com>
2024-10-05 12:00:43 -0300

    Se crean componentes ChildView y ParentView y se desarrolla su logica

---
+++ src/components/ChildView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" v-model="inputChild" placeholder="Escribe tu mensaje">
+        <button @click="sendText">Enviar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputChild: ''
+        }
+    },
+    methods: {
+        sendText() {
+            this.$emit('send-text', this.inputChild);
+            this.inputChild = '';
+        }
+    }
+}
+</script>
---
+++ src/components/ParentView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Texto recibido de ChildView: </h1>
+        <p>{{ recivedText }}</p>
+    <ChildView @send-text="recivedText"/> 
+</div>
+</template>
+
+<script>
+import ChildView from "./ChildView.vue";
+
+export default {
+    name: "ParentView",
+    components: {
+        ChildView
+    },
+    data() {
+        return {
+            recivedText: ""
+        }
+    },
+    methods: {
+        recivedText(text) {
+            this.recivedText = text;
+        }
+    }
+}
+</script>
