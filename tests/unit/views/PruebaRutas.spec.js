a229cc0fb5777cb6dbf7e952ff33395dd5aefed5
Constanza <csdelartundo@gmail.com>
2024-10-07 10:45:13 -0300

    Docs: se agregan comentarios explicativos en las pruebas de rutas para HomeView y AboutView

--- tests/unit/views/PruebaRutas.spec.js
+++ tests/unit/views/PruebaRutas.spec.js
@@ -4,44 +4,53 @@ import AboutView from '@/views/AboutView.vue'
 import HomeView from '@/views/HomeView.vue'
 
 describe('Vistas HomeView y AboutView', () => {
+    // Grupo de pruebas para las vistas HomeView y AboutView
+
     test('Probando existencia de vista HomeView', async () => {
+        // Prueba para verificar la existencia de la vista 'HomeView'
+
         const router = createRouter({
-            history: createWebHistory(),
+            history: createWebHistory(), // Crea un historial web usando Vue Router
             routes: [
                 {
-                    path: '/',
-                    name: 'home',
-                    component: HomeView
+                    path: '/', // Ruta para la vista Home
+                    name: 'home', // Nombre de la ruta
+                    component: HomeView // Componente HomeView
                 }
             ]
         })
-        router.push('/')
-        await router.isReady()
+        router.push('/') // Navega a la ruta '/'
+        await router.isReady() // Espera que el router esté listo
         const wrapper = mount(HomeView, {
             global: {
-                plugins: [router]
+                plugins: [router] // Agrega el router como plugin global
             }
         })
+        // Verifica que el componente HomeView exista en el DOM
         expect(wrapper.findComponent(HomeView).exists()).toBe(true)
     })
+
     test('Probando existencia de vista AboutView', async () => {
+        // Prueba para verificar la existencia de la vista 'AboutView'
+
         const router = createRouter({
-            history: createWebHistory(),
+            history: createWebHistory(), // Crea un historial web usando Vue Router
             routes: [
                 {
-                    path: '/about',
-                    name: 'about',
-                    component: AboutView
+                    path: '/about', // Ruta para la vista About
+                    name: 'about', // Nombre de la ruta
+                    component: AboutView // Componente AboutView
                 }
             ]
         })
-        router.push('/')
-        await router.isReady()
+        router.push('/') // Navega a la ruta '/'
+        await router.isReady() // Espera que el router esté listo
         const wrapper = mount(AboutView, {
             global: {
-                plugins: [router]
+                plugins: [router] // Agrega el router como plugin global
             }
         })
+        // Verifica que el componente AboutView exista en el DOM
         expect(wrapper.findComponent(AboutView).exists()).toBe(true)
     })
 })
